3071f997a9b1b85d6d3d4ef187effa880f27f751
hieubdn <hieubdn@gmail.com>
2024-10-30 18:32:22 +0700

    update

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Cấu hình Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDXDufpv9M0gqLKTO3Gzx2ur2-pKRpcUX0",
-  authDomain: "tattothetrung.firebaseapp.com",
-  projectId: "tattothetrung",
-  storageBucket: "tattothetrung.appspot.com",
-  messagingSenderId: "870589023967",
-  appId: "1:870589023967:web:f9f93a172494aeb70c4b6a",
-  measurementId: "G-0VLQMBK74J"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Khởi tạo Firebase
